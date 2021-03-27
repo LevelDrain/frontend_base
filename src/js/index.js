56ee45e6a41dd1d02162f8bf9c2f989314d976e1
LevelDrain <drain2291@gmail.com>
2021-03-27 12:15:19 +0900

    階層構造の変更

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 const init = () => {
   const $place = document.querySelector('#Place');
-  $place.innerHTML = 'フロントエンド基本環境テスト用';
+  $place.innerHTML = 'フロントエンド基本開発環境テスト';
 };
 
 onload = init;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,48 @@
 module.exports = {
-  //mode: "production",
-  mode: "development",
+  mode: 'production',
+  //mode: 'development',
   devServer: {
-    contentBase: "dist",
-    open: true
+    contentBase: 'dist',
+    open: true,
+    hot: true,
   },
 
-  entry: ["@babel/polyfill", "./src/index.js"],
+  entry: ['@babel/polyfill', './src/js/index.js'],
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: `${__dirname}/dist`,
+    path: `${__dirname}/dist/js`,
+    publicPath: '/js/',
     // 出力ファイル名
-    filename: "main.js",
+    filename: 'main.js',
   },
   module: {
-    rules: [{
-      // 拡張子 .js の場合
-      test: /\.js$/,
-      use: [{
-        // Babel を利用する
-        loader: "babel-loader",
-        // Babel のオプションを指定する
-        options: {
-          presets: [
-            // プリセットを指定することで、ES2020 を ES5 に変換
-            [
-              "@babel/preset-env",
-              {
-                useBuiltIns: "entry",
-                corejs: 3,
-              },
-            ],
-          ],
-        },
-      }, ],
-    }, ],
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2020 を ES5 に変換
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'entry',
+                    corejs: 3,
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+    ],
   },
   // ES5(IE11等)向けの指定
-  target: ["web", "es5"],
+  target: ['web', 'es5'],
 };
